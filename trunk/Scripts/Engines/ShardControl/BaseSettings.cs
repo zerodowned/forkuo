917a7d6bb4caa2b6e178b3590f5a47eff4bd96c5
augmentedinsanity@gmail.com <augmentedinsanity@gmail.com@6b3a697c-6396-a1a8-7fb3-172c641f22c5>
2012-09-29 03:52:03 +0000

    + Added EmergencyBackup project  - Simple console application designed to do an emergency backup for ForkUO  - Supports 7zip compression  - Accepts 3 arguments <save path> <backup path> <compression level> + Added the beginnings of the Shard Control system  - Just a collection of settings atm, will expand into a system that allows full control over these settings

---
+++ trunk/Scripts/Engines/ShardControl/BaseSettings.cs
@@ -0,0 +1,12 @@
+﻿using Server;
+
+namespace CustomsFramework.Systems.ShardControl
+{
+    public abstract class BaseSettings
+    {
+        public abstract override string ToString();
+
+        protected abstract void Serialize(GenericWriter writer);
+        protected abstract void Deserialize(GenericReader reader);
+    }
+}
